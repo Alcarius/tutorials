8f4b87d75010ba154fc5ca91c621c25688b7d0bc
arthur <arthur.roenfeld@novalabs.at>
2026-04-08 10:09:57 +0200

    created the manifest and init for an estate module as part of the odoo server framework 101 tutorial

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'summary': 'Real Estate Management Module',
+    'description': 'A training module for managing real estate properties.',
+    'category': 'Real Estate',
+    'author': 'Arthur Rönfeld',
+    'website': 'https://www.example.com',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        # Add XML files for views, menus, etc., e.g., 'views/estate_views.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
